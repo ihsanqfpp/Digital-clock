95772851414ca4e2d1aa50ccaee4438f8fc74644
ihsanqfpp <okzihsan956@gmail.com>
2024-11-26 08:38:55 +0500

    add the index

---
+++ script.js
@@ -0,0 +1,14 @@
+function clock (){
+    let now = new Date()
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? "0" +hours : hours
+    minutes = minutes < 10 ? "0" +minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+    let time = hours + ":" +minutes + ":" +seconds
+
+    document.getElementById("clock").innerHTML = time
+}
+setInterval(clock, 1000);
